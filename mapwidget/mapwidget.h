d664e839e8d55f86840c7a8ae5f05f81cee0b60d
felipe.sanchezarenas <felipe.sanchezarenas@alum.uca.es>
2014-12-12 17:07:24 +0100

    corregido error en mapwidget.h (definicion de ámbito MapWidget:: eliminada porque no se pone en .h). COMPILAD ANTES DE SUBIR A REPOSITORIO

--- mapwidget/mapwidget.h
+++ mapwidget/mapwidget.h
@@ -22,7 +22,7 @@ public:
     void paintOrigin(const Origin& origin);
     void paintStations(const std::set<Station>& stations);
     void paintCircles();
-    void MapWidget::paintCircles(const long double& x, const long double& y, const long double& radius, int transparence = 0 , int red = 256, int green = 256, int blue = 256);
+    void paintCircles(const long double& x, const long double& y, const long double& radius, int transparence = 0 , int red = 256, int green = 256, int blue = 256);
 private:
     Ui::MapWidget *ui;
     QImage mapImage;
