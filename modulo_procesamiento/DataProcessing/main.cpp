998dbacc8b2793ce74b63fc6c5aee4046b2e5b49
Andrés Jesús Sánchez Anillo <Andressanchezanillo@gmail.com>
2015-01-19 22:56:18 +0100

    Actualizacion 19/01/2015 23:00:00

--- modulo_procesamiento/DataProcessing/main.cpp
+++ modulo_procesamiento/DataProcessing/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QXmlStreamReader>
 #include "dataprocessing.h"
 
 
@@ -6,18 +7,19 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     DataProcessing processing;
+    processing.ProcessOriginFromFileXml(":/testFiles/gfz2014dibx.last.xml");
 
     //First instrucction.
-    processing.ProcessStationsFromFile(":/testFiles/station.txt");
+    //processing.ProcessStationsFromFile(":/testFiles/station.txt");
 
     // Those instruction will be concurrent called.
-    processing.ProcessAnyFile(":/testFiles/scalertes_picks.log");
-    processing.ProcessAnyFile(":/testFiles/origin.txt");
-    processing.ProcessAnyFile(":/testFiles/gfz2014dibx.last.xml");
+    //processing.ProcessAnyFile(":/testFiles/scalertes_picks.log");
+    //processing.ProcessAnyFile(":/testFiles/origin.txt");
+    //processing.ProcessAnyFile(":/testFiles/gfz2014dibx.last.xml");
 
-    std::set<Station> stations(processing.getStations());
-    Origin origen(processing.getOrigin());
-    origen.setStations(stations);
-    std::cout << origen << std::endl;
+    //std::set<Station> stations(processing.getStations());
+    //Origin origen(processing.getOrigin());
+    //origen.setStations(stations);
+    //std::cout << origen << std::endl;
     return a.exec();
 }
